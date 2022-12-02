48e288757401250c5cb09b14b8de81f9a1a50c08
Abd-El-Rahman Dwidar <34503868+AbdoDwidar97@users.noreply.github.com>
2022-12-02 18:31:56 +0200

    Solving [684. Redundant Connection - Graph - Medium] with (Union Find Algorithm).

---
+++ src/Graphs/RedundantConnection.java
@@ -0,0 +1,68 @@
+package Graphs;
+
+import java.util.Arrays;
+
+public class RedundantConnection
+{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        // int [] edge = solution.findRedundantConnection(new int[][]{{1,2}, {1,3}, {2,3}});
+        int [] edge2 = solution.findRedundantConnection(new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}});
+
+        Arrays.stream(edge2).forEach(System.out::println);
+    }
+
+    private static class Solution
+    {
+        /// Solving this problem with union find algorithm.
+        private int[] parents;
+        public int[] findRedundantConnection(int[][] edges)
+        {
+            for (int i = edges.length - 1; i >= 0; i--)
+            {
+                int components = countComponents(edges, i);
+                if (components == 1) return edges[i];
+            }
+
+            return edges[0];
+        }
+
+        private int countComponents(int [][] edges, int ignoredEdgeIndex)
+        {
+            int numOfComponents = edges.length;
+            parents = new int[edges.length];
+            for (int i = 0; i < parents.length; i++) parents[i] = i;
+
+            for (int i = 0; i < edges.length; i++)
+            {
+                if (i != ignoredEdgeIndex)
+                {
+                    if (find(edges[i][0] - 1) != find(edges[i][1] - 1))
+                    {
+                        union(edges[i][1] - 1, edges[i][0] - 1);
+                        numOfComponents--;
+                    }
+                }
+            }
+
+            return numOfComponents;
+        }
+
+        private int find(int node)
+        {
+            return parents[node];
+        }
+
+        private void union(int node, int parent)
+        {
+            int newParentVal = parents[parent];
+            int currentNodeVal = parents[node];
+
+            for (int i = 0; i < parents.length; i++)
+            {
+                if (parents[i] == currentNodeVal) parents[i] = newParentVal;
+            }
+        }
+    }
+}
