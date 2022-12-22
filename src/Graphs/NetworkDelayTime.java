d6549f14ca3ff7501f810a9268cb92d7d69d375f
Abd-El-Rahman Dwidar <34503868+AbdoDwidar97@users.noreply.github.com>
2022-12-22 10:21:03 +0200

    Solving [743. Network Delay Time] - Graph - Medium...

---
+++ src/Graphs/NetworkDelayTime.java
@@ -0,0 +1,19 @@
+package Graphs;
+
+public class NetworkDelayTime
+{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        System.out.println(solution.networkDelayTime(new int[][]{{2,1,1},{2,3,1},{3,4,1}}, 4, 2));
+    }
+
+    private static class Solution
+    {
+        public int networkDelayTime(int[][] times, int n, int k)
+        {
+            /// write solution here ...
+            return 0;
+        }
+    }
+}
