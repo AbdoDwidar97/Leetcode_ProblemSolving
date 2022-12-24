b0db70e6ca5f340726ba26bca3796ae4612f22b8
Abd-El-Rahman Dwidar <34503868+AbdoDwidar97@users.noreply.github.com>
2022-12-24 18:40:33 +0200

    Working on [210. Course Schedule II] - Graph - Medium III ...

--- src/Graphs/CourseScheduleII.java
+++ src/Graphs/CourseScheduleII.java
@@ -1,15 +1,20 @@
 package Graphs;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Queue;
+import java.util.*;
 
 public class CourseScheduleII
 {
     public static void main(String[] args)
     {
-        // Working on ...
+        Solution solution = new Solution();
+
+        /*int[] res = solution.findOrder(4, new int[][]{{1,0},{2,0},{3,1},{3,2}});
+        Arrays.stream(res).forEach(System.out::println);*/
+
+        /// this example should return empty array, but the result is wrong ....
+        int[] res = solution.findOrder(2, new int[][]{{1,0},{0,1}});
+        Arrays.stream(res).forEach(System.out::println);
+
     }
 
     private static class Solution
@@ -17,43 +22,53 @@ public class CourseScheduleII
         int[] parents;
         public int[] findOrder(int numCourses, int[][] prerequisites)
         {
-            ArrayList<Integer> coursesOrder = new ArrayList<>();
+            ArrayList<Integer> courses = new ArrayList<>();
 
-            parents = new int[numCourses];
-            HashSet<Integer> availCourses = new HashSet<>();
-            Queue<Integer> unvisited = new ArrayDeque<>();
-            ArrayList<Integer>[] graph = new ArrayList[numCourses];
-
-            for (int i = 0; i < numCourses; i++)
+            if (prerequisites.length == 0)
             {
-                parents[i] = i;
-                availCourses.add(i);
-                graph[i] = new ArrayList<>();
+                for (int i = 0; i < numCourses; i++) courses.add(i);
+
+                return courses.stream().mapToInt(Integer::intValue).toArray();
             }
 
-            graph = createGraph(prerequisites, graph);
+            ArrayList<Integer>[] graph = createGraph(numCourses, prerequisites);
+            HashSet<Integer> visited = new HashSet<>();
+            Queue<Integer> unvisited = new ArrayDeque<>();
 
             unvisited.add(find(0));
 
             while (!unvisited.isEmpty())
             {
-                int root = unvisited.poll();
-                availCourses.remove(root);
-                coursesOrder.add(root);
+                int currentCourse = unvisited.poll();
 
-                ArrayList<Integer> children = graph[root];
-
-                for (int child : children)
+                if (!visited.contains(currentCourse))
                 {
-                    if (availCourses.contains(child)) unvisited.add(child);
+                    visited.add(currentCourse);
+                    courses.add(currentCourse);
+
+                    ArrayList<Integer> children = graph[currentCourse];
+
+                    for (int child : children)
+                    {
+                        if (!visited.contains(child)) unvisited.add(child);
+                    }
                 }
             }
 
-            return new int[]{};
+            return courses.stream().mapToInt(Integer::intValue).toArray();
         }
 
-        private ArrayList<Integer>[] createGraph(int[][] prerequisites, ArrayList<Integer>[] graph)
+        private ArrayList<Integer>[] createGraph(int numCourses, int[][] prerequisites)
         {
+            ArrayList<Integer>[] graph = new ArrayList[numCourses];
+            parents = new int[numCourses];
+
+            for (int i = 0; i < numCourses; i++)
+            {
+                graph[i] = new ArrayList<>();
+                parents[i] = i;
+            }
+
             for (int[] edge : prerequisites)
             {
                 int a = edge[0];
@@ -70,7 +85,7 @@ public class CourseScheduleII
         private int find(int node)
         {
             if (parents[node] == node) return parents[node];
-            else return find(parents[node]);
+            return find(parents[node]);
         }
 
         private void union(int node, int parent)
