f10d6627d2903593d109fda925e36f49286cd181
a.dwidar <wfzaoaonisjavltvkyay6qtlqia6xoguus6e3lidplsdydv4puta>
2022-12-27 14:52:27 +0200

    Solved [210. Course Schedule II] - Graph - Medium.

--- src/Graphs/CourseScheduleII.java
+++ src/Graphs/CourseScheduleII.java
@@ -8,66 +8,45 @@ public class CourseScheduleII
     {
         Solution solution = new Solution();
 
-        /*int[] res = solution.findOrder(4, new int[][]{{1,0},{2,0},{3,1},{3,2}});
-        Arrays.stream(res).forEach(System.out::println);*/
+        int[] res = solution.findOrder(4, new int[][]{{1,0},{2,0},{3,1},{3,2}});
+        Arrays.stream(res).forEach(System.out::println);
 
         /// this example should return empty array, but the result is wrong ....
-        int[] res = solution.findOrder(2, new int[][]{{1,0},{0,1}});
-        Arrays.stream(res).forEach(System.out::println);
+        /*int[] res = solution.findOrder(2, new int[][]{{1,0},{0,1}});
+        Arrays.stream(res).forEach(System.out::println);*/
 
     }
 
     private static class Solution
     {
-        int[] parents;
+        private HashSet<Integer> availCourses = new HashSet<>();
         public int[] findOrder(int numCourses, int[][] prerequisites)
         {
-            ArrayList<Integer> courses = new ArrayList<>();
-            HashSet<Integer> availCourses = new HashSet<>();
+            ArrayList<Integer>[] graph = createGraphAndSetupAvailCourses(numCourses, prerequisites);
 
-            for (int i = 0; i < numCourses; i++) availCourses.add(i);
+            ArrayList<Integer> result = new ArrayList<>();
 
-            ArrayList<Integer>[] graph = createGraph(numCourses, prerequisites);
-
-            if (findCycle(graph, find(0), new HashSet<>())) return new int[]{};
-
-            HashSet<Integer> visited = new HashSet<>();
-            Queue<Integer> unvisited = new ArrayDeque<>();
-
-            unvisited.add(find(0));
-
-            while (!unvisited.isEmpty())
+            while (!availCourses.isEmpty())
             {
-                int currentCourse = unvisited.poll();
+                int start = availCourses.stream().findFirst().get();
+                ArrayList<Integer> res = dfsFindOrder(graph, start, new HashSet<>());
 
-                if (!visited.contains(currentCourse))
-                {
-                    visited.add(currentCourse);
-                    courses.add(currentCourse);
-                    availCourses.remove(currentCourse);
+                if (res.isEmpty()) return new int[]{};
 
-                    ArrayList<Integer> children = graph[currentCourse];
-
-                    for (int child : children)
-                    {
-                        if (!visited.contains(child)) unvisited.add(child);
-                    }
-                }
+                result.addAll(res);
             }
 
-            courses.addAll(availCourses);
-            return courses.stream().mapToInt(Integer::intValue).toArray();
+            return result.stream().mapToInt(Integer::intValue).toArray();
         }
 
-        private ArrayList<Integer>[] createGraph(int numCourses, int[][] prerequisites)
+        private ArrayList<Integer>[] createGraphAndSetupAvailCourses(int numCourses, int[][] prerequisites)
         {
             ArrayList<Integer>[] graph = new ArrayList[numCourses];
-            parents = new int[numCourses];
 
             for (int i = 0; i < numCourses; i++)
             {
                 graph[i] = new ArrayList<>();
-                parents[i] = i;
+                availCourses.add(i);
             }
 
             for (int[] edge : prerequisites)
@@ -75,41 +54,34 @@ public class CourseScheduleII
                 int a = edge[0];
                 int b = edge[1];
 
-                graph[b].add(a);
-
-                if (find(a) != find(b)) union(a, b);
+                graph[a].add(b);
             }
 
             return graph;
         }
 
-        private int find(int node)
-        {
-            if (parents[node] == node) return parents[node];
-            return find(parents[node]);
-        }
-
-        private void union(int node, int parent)
-        {
-            int nodeRoot = find(node);
-            int parentRoot = find(parent);
-
-            parents[nodeRoot] = parentRoot;
-        }
-
-        private boolean findCycle(ArrayList<Integer>[] graph, int currentNode, HashSet<Integer> visited)
+        private ArrayList<Integer> dfsFindOrder(ArrayList<Integer>[] graph, int currentNode, HashSet<Integer> visited)
         {
             HashSet<Integer> myVisited = new HashSet<>(visited);
             myVisited.add(currentNode);
 
             ArrayList<Integer> children = graph[currentNode];
 
-            boolean result = false;
-            for (int child : children)
+            ArrayList<Integer> result = new ArrayList<>();
+            for (int course : children)
             {
-                if (myVisited.contains(child)) return true;
-                else result = result || findCycle(graph, child, myVisited);
+                if (visited.contains(course)) return new ArrayList<>();
+                if (availCourses.contains(course))
+                {
+                    ArrayList<Integer> res = dfsFindOrder(graph, course, myVisited);
+                    if (res.isEmpty()) return res;
+
+                    result.addAll(res);
+                }
             }
+
+            result.add(currentNode);
+            availCourses.remove(currentNode);
 
             return result;
         }
