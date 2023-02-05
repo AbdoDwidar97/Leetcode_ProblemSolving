86717a5f8bcfd12d624f592d1824e1309e1b6849
a.dwidar <wfzaoaonisjavltvkyay6qtlqia6xoguus6e3lidplsdydv4puta>
2023-02-05 10:19:07 +0200

    Solved [1504. Count Submatrices With All Ones ] - DP - Medium.

---
+++ src/DynamicProgramming/AllPossibleFullBinaryTrees.java
@@ -0,0 +1,34 @@
+package DynamicProgramming;
+
+import java.util.List;
+
+public class AllPossibleFullBinaryTrees
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static class Solution
+    {
+        public List<TreeNode> allPossibleFBT(int n)
+        {
+            return null;
+        }
+    }
+
+    private static class TreeNode
+    {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+}
