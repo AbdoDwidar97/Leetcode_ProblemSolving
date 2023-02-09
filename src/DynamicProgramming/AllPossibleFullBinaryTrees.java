394f5e32fc324cacc1bbcc0ff40f47112720ec11
a.dwidar <wfzaoaonisjavltvkyay6qtlqia6xoguus6e3lidplsdydv4puta>
2023-02-09 16:58:25 +0200

    Solved [1641. Count Sorted Vowel Strings] - DP - Medium.

--- src/DynamicProgramming/AllPossibleFullBinaryTrees.java
+++ src/DynamicProgramming/AllPossibleFullBinaryTrees.java
@@ -1,5 +1,6 @@
 package DynamicProgramming;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AllPossibleFullBinaryTrees
@@ -13,7 +14,49 @@ public class AllPossibleFullBinaryTrees
     {
         public List<TreeNode> allPossibleFBT(int n)
         {
+            TreeNode[] table = new TreeNode[n + 1];
+
             return null;
+        }
+
+        private List<TreeNode> createTrees(TreeNode root, TreeNode[] table, List<TreeNode> trees, int n)
+        {
+            /// First, we should copy Root tree
+            TreeNode myRoot = copyTree(root);
+
+            /// Second, we should make a new root node belongs to n level, and then, fill left and right of this node, by recursion. // EX:
+            TreeNode myNodeRoot = new TreeNode(0);
+
+            /// we must have List<TreeNode> for all possible trees according n level. // EX:
+            List<TreeNode> myRootPossibleList = new ArrayList<>();
+
+            int remain = n - 1;
+            int leftNum = 1, rightNum = remain - leftNum;
+
+            while (leftNum <= rightNum)
+            {
+                if (table[leftNum] == null) continue; /// ----
+                if (table[rightNum] == null) continue; /// ----
+
+                leftNum += 2;
+                rightNum -= 2;
+            }
+
+            return trees;
+        }
+
+        private TreeNode copyTree(TreeNode root)
+        {
+            if (root == null) return null;
+
+            TreeNode myRoot = new TreeNode(root.val);
+
+            if (root.left == null && root.right == null) return myRoot;
+
+            myRoot.left = copyTree(root.left);
+            myRoot.right = copyTree(root.right);
+
+            return myRoot;
         }
     }
 
