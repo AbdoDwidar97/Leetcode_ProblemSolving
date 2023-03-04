db25922c89e9375b576a1df9a8cf0c0ce0a17278
Abd-El-Rahman Dwidar <34503868+AbdoDwidar97@users.noreply.github.com>
2023-03-04 14:08:28 +0200

    morning--

---
+++ src/DynamicProgramming/AllPossibleFullBinaryTrees.java
@@ -0,0 +1,35 @@
+package DynamicProgramming;
+
+import java.util.List;
+
+public class AllPossibleFullBinaryTrees
+{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        List<TreeNode> res = solution.allPossibleFBT(7);
+    }
+
+    private static class Solution
+    {
+        public List<TreeNode> allPossibleFBT(int n)
+        {
+            return null;
+        }
+    }
+
+    private static class TreeNode
+    {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right)
+          {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+    }
+}
